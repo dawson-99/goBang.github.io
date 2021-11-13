80d209f0808d9f260097d01767e1f6861d70ad1c
徐睿航 <iruihang@MacBook-Pro.local>
2021-11-13 16:22:49 +0800

    impl

--- goBang/src/checkBoardService/checkBoardServiceImpl/judgeServiceImpl.java
+++ goBang/src/checkBoardService/checkBoardServiceImpl/judgeServiceImpl.java
@@ -3,11 +3,16 @@ package checkBoardService.checkBoardServiceImpl;
 import checkBoardService.judgeService;
 import com.goBang.model.Board;
 
+//此实现主要用于判断是否有胜利，是否禁手等操作
+//此算法中，默认白方一定可以辨别出黑方禁手，一旦判定黑方禁手，则黑方输
 public class judgeServiceImpl implements judgeService {
 
     //1代表黑棋、2代表白棋、0代表此处空
+    //禁手概念：黑棋一子落下，同时形成三三、或者四四、或者长连，且没有形成五连。那么，这个点就是禁手点，黑棋判负。白棋没有禁手。
+
     public int judge(Board board, int x, int y) {
 
+        
         return 0;
     }
 }
