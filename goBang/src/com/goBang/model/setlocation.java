2235f5b6a1225dd029d726c49b9fe896e6e1b812
uer <sityhitty@gmail.com>
2021-11-20 16:27:45 +0800

    delete

--- goBang/src/com/goBang/model/setlocation.java
+++ goBang/src/com/goBang/model/setlocation.java
@@ -1,10 +1,5 @@
 package com.goBang.model;
-
-import AIService.AIService;
-import AIService.AIServiceImpl.AIServiceImpl;
-
 public class setlocation {
     public Board f;
-    AIService AI=new AIServiceImpl();
     //AI
 }
