58587e35ad2f6e7fda11586a59a8abfcbb4fd6b6
shasha <nightyfury3@gmail.com>
2021-12-24 14:21:50 +0800

    Ai

--- goBang/src/AIService/AIService.java
+++ goBang/src/AIService/AIService.java
@@ -4,5 +4,6 @@ import java.awt.*;
 public interface AIService {
     public void Find(Board board);
     public void InitSCore(Board board, char AI,char player);
+    public void InitSCore_2(Board board, char AI,char player);
     public Point Max();
 }
