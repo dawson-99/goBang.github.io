70b245b018743bdde63d6a59f77ba4e501ef971c
uer <sityhitty@gmail.com>
2021-12-24 20:58:25 +0800

    ab

--- goBang/src/com/goBang/model/AI.java
+++ goBang/src/com/goBang/model/AI.java
@@ -1,33 +1,61 @@
 package com.goBang.model;
-import AIService.AIServiceImpl.AIwork;
 import checkBoardService.size;
-import AIService.*;
-
 import java.awt.*;
+import java.util.ArrayList;
 
 public class AI implements size {
     int evaluation;
+    int aiturn;
+    int depth=4;
+    Board board;
+    char[][] isavil = new char[row][column];
     evaluated ed;
-    public void Alpha_Beta(Board board) {
-        char[][] isavil = new char[row][column];
+    AI(Board board){
+        this.board=board;
         for (int i = 0; i < row; i++) {
             isavil[i] = board.isolation[i].clone();
         }
-        int Alpha = Integer.MAX_VALUE, Beta = Integer.MIN_VALUE;
-        AIService aiService = new AIwork();
-        aiService.Find(board);
-        aiService.InitSCore(board, '2', (char) board.turn);
-        Point AIp = aiService.Max();
-        setchess(isavil,AIp, (char) board.turn);
-        int deepth=0;
-        /*while (deepth<4){
-            evaluation = ed.evaluate(isavil);
-            Aa=evaluation;
-            if(){
+    }
 
+    public int  MINMAX(Board board) {
+        if(board.turn==aiturn){
+            return MAX(depth);
+        }else{
+            return MIN(depth);
+        }
+    }
+    public int  MAX(int depth){
+        int MAX=Integer.MIN_VALUE,cot=0;
+        if(depth<0){
+            return ed.evaluate(isavil);
+        }else{
+            while(makenextmove(isavil,(char)aiturn,cot++)) {
+               var val=MIN(depth-1);
+               if(val>MAX)MAX=val;
             }
         }
-         */
+        return MAX;
+    }
+    public int  MIN(int depth){
+        int MIN=Integer.MAX_VALUE;
+        int cot=0;
+        if(depth<0){
+            return ed.evaluate(isavil);
+        }else{
+            while(makenextmove(isavil,(char)aiturn,cot++)){
+                var val=MAX(depth-1);
+                if(val<MIN)MIN=val;
+            }
+        }
+        return MIN;
+    }
+    public boolean makenextmove(char isavil[][],char turn,int cot){
+        if(cot%15==14) return false;
+        if (isavil[cot/(row-1)][cot%column]=='0'){
+            isavil[cot/(row)-1][cot%column]=turn;
+        }
+        return true;
+
     }
     public void setchess( char[][] isavil,Point AIp,char turn){
         isavil[AIp.x][AIp.y]=turn;
